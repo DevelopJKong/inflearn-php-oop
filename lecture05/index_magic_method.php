bfbfad40f51857472d6301baa756bb8ce9a634c7
DevelopJKong <ybaram40@gmail.com>
2021-12-23 01:03:05 +0900

    06 - 매직 메서드 (Magic Methods) 학습중

---
+++ lecture05/index_magic_method.php
@@ -0,0 +1,37 @@
+<?php
+//magic method 에서 가장 중요한것은 생성자와 소멸자이다
+/**
+ * Magic Methods: Methods
+ */
+class A
+{
+    // __call 이라는 매직 메소드는 정의 되지 않은  메소드를 호출할때 대신 호출 된다
+    public function __call($name,$args) {
+        
+        //var_dump(__METHOD__);
+        var_dump($name,$args);
+
+    }
+    // 1
+    //만약에 여기서 foo() 가 주석이 되어 있으면 foo() 가 호출되는것이 아니라 __call이 호출이 된다
+    // public function foo() {
+    //     var_dump(__METHOD__);
+    // }
+
+    // 2
+    public static function __callStatic($name,$args) {
+        var_dump($name,$args);
+    }
+
+    public function __invoke( ... $args) {
+        var_dump($args);
+    }
+}
+
+$a = new A();
+$a('Hello,world','Who are you?');
+
+
+//$a -> foo();
+//$a -> foo('Hello,world');
+//A::foo(); // => 정확하게 :: 는 무슨 뜻인지 알아두기
